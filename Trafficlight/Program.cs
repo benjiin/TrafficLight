19b2f8d51011023ebd8fb8092ae4106b2a7be9c8
ben <nemewesa@gmail.com>
2017-04-28 09:11:48 +0200

    .

--- Trafficlight/Program.cs
+++ Trafficlight/Program.cs
@@ -4,12 +4,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Trafficlight
+namespace Aufgabe2
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Crossroads crossroads = new Crossroads();
+            if (crossroads.SignalN.Color == Signal.GREEN)
+            {
+                Console.WriteLine("N: Green");
+            }
+            else if (crossroads.SignalN.Color == Signal.RED)
+            {
+                Console.WriteLine("N: Red");
+            }
+            if (crossroads.SignalS.Color == Signal.GREEN)
+            {
+                Console.WriteLine("S: Green");
+            }
+            else if (crossroads.SignalS.Color == Signal.RED)
+            {
+                Console.WriteLine("S: Red");
+            }
+
+            if (crossroads.SignalW.Color == Signal.GREEN)
+            {
+                Console.WriteLine("W: Green");
+            }
+            else if (crossroads.SignalW.Color == Signal.RED)
+            {
+                Console.WriteLine("W: Red");
+            }
+
+            if (crossroads.SignalO.Color == Signal.GREEN)
+            {
+                Console.WriteLine("O: Green");
+            }
+            else if (crossroads.SignalO.Color == Signal.RED)
+            {
+                Console.WriteLine("O: Red");
+            }
+
+            crossroads.Switch();
+            Console.WriteLine("-");
+
+            if (crossroads.SignalN.Color == Signal.GREEN)
+            {
+                Console.WriteLine("N: Green");
+            }
+            else if (crossroads.SignalN.Color == Signal.RED)
+            {
+                Console.WriteLine("N: Red");
+            }
+            if (crossroads.SignalS.Color == Signal.GREEN)
+            {
+                Console.WriteLine("S: Green");
+            }
+            else if (crossroads.SignalS.Color == Signal.RED)
+            {
+                Console.WriteLine("S: Red");
+            }
+
+            if (crossroads.SignalW.Color == Signal.GREEN)
+            {
+                Console.WriteLine("W: Green");
+            }
+            else if (crossroads.SignalW.Color == Signal.RED)
+            {
+                Console.WriteLine("W: Red");
+            }
+
+            if (crossroads.SignalO.Color == Signal.GREEN)
+            {
+                Console.WriteLine("O: Green");
+            }
+            else if (crossroads.SignalO.Color == Signal.RED)
+            {
+                Console.WriteLine("O: Red");
+            }
+            Console.ReadLine();
         }
     }
 }
