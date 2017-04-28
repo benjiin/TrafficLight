351398d27f3ba1fa2282e7b88e8f1e922e1de1d9
ben <nemewesa@gmail.com>
2017-04-28 09:38:53 +0200

    .

---
+++ Trafficlight/Crossroads.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trafficlight
+{
+    class Crossroads
+    {
+        Signal SignalN = new Signal();
+        Signal SignalS = new Signal();
+        Signal SignalW = new Signal();
+        Signal SignalO = new Signal();
+
+        public Crossroads()
+        {
+
+        }
+        public void Switch()
+        {
+            Console.WriteLine("wsw");
+        }
+    }
+}
